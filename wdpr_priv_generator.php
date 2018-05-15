8a27cdae01d731acf8b248b7c283ff3f0bef51ad
Cinemapub <support@cinemapub.be>
2018-05-15 15:35:32 +0000

    allow for multiple templates

--- wdpr_priv_generator.php
+++ wdpr_priv_generator.php
@@ -3,13 +3,14 @@
 Plugin Name: WDPR Privacy Policy Generator
 Plugin URI:  
 Description: [wdpr_priv_generator] shortcode
-Version: 1.0
+Version: 1.1
 Author: pforret
 Author URI: https://www.wdpr.eu
 License: GPLv3
 */
-require_once dirname( __FILE__ ) . '/lib/wdpr_fieldconfig.php';
-require_once dirname( __FILE__ ) . '/lib/wdpr_textgenerator.php';
+require_once dirname(__FILE__) . '/lib/wdpr_questions.php';
+require_once dirname(__FILE__) . '/lib/wdpr_answers.php';
+require_once dirname(__FILE__) . '/lib/wdpr_tools.php';
 
 define('WDPR_PRIV_GENERATOR_PLUGIN_VERSION', '1.0');
 
@@ -35,19 +36,20 @@ if ( !function_exists('wdpr_priv_generator_shortcode') ) {
 
 	function wdpr_priv_generator_shortcode( $atts ) {
 		global $post, $wdprpg_settings;
-        $defaults=Array("lang" => "en");
+		$tool=New wdpr_tools();
+        $defaults=Array(
+            "lang" => "en",
+            "template"  => "privacy");
         extract( shortcode_atts( $defaults, $atts ) );
-        $ini_file=dirname( __FILE__ ) . "/lang/fields-$lang.ini";
+        $ini_file=$tool->pick_ini($template,$lang);
+        //$ini_file=dirname( __FILE__ ) . "/lang/fields-$lang.ini";
         if(!file_exists($ini_file)){
             $return="<p class='notice notice-error'>Language [$lang] not supported (yet).</p>\n";
             return $return;
         }
-        $wdpr_fields=New wdpr_fieldconfig($ini_file); // make this multi lingual later
+        $wdpr_fields=New wdpr_questions($ini_file); // make this multi lingual later
 
 		$fields=$wdpr_fields->get_postfields("wdpr_");
- 		echo "<!--";
-		print_r($fields);
-		echo "-->\n";
 		$status="FILLIN";
 		if($fields["_submitted"]){
 			if($fields["_form_has_validation_errors"]){
@@ -58,27 +60,30 @@ if ( !function_exists('wdpr_priv_generator_shortcode') ) {
 		}
 		switch($status){
 			case "FILLIN":
-			echo "<!-- FILL IN FORM -->\n";
-            $return=$wdpr_fields->show_form("wdpr_");
-			break;
+                $tool->trace("FILL IN FORM");
+                $return=$wdpr_fields->show_form("wdpr_");
+                break;
 			
 			case "ERRORS":
-			echo "<!-- VALIDATION ERRORS -->\n";
-			$return=$wdpr_fields->show_validation_errors("wdpr_");
-			break;
+                $tool->trace("VALIDATION ERRORS");
+                $return=$wdpr_fields->show_validation_errors("wdpr_");
+                break;
 
 			case "PROCESS":
-			echo "<!-- PROCESS FORM -->\n";
-			$wdpr_gen=New wdpr_textgenerator();
-			$template="privacy";
-			$lang=$fields["lang"];
-			$ini_file=dirname( __FILE__ ) . "/lang/$template-$lang.ini";
-
-			$return=$wdpr_gen->generate($ini_file,$fields);
-			break;
+                $tool->trace("PROCESS FORM");
+                $wdpr_gen=New wdpr_answers();
+                $lang=$fields["lang"];
+                $ini_file=$tool->pick_ini($template,$lang,true);
+                if($ini_file){
+                    $return=$wdpr_gen->generate($ini_file,$fields);
+                } else {
+                    $return="<!-- template/lang not found -->";
+                }
+                break;
 			
 		default:
-			echo "<!-- NOT SURE WHAT TO DO -->\n";
+            $tool->trace("NO MATCH");
+			$return="<!-- NOT SURE WHAT TO DO -->";
 		}
 		return $return;
 	}
